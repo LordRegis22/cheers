012b47513b99ba8c96e93686e56a831934ca7b52
Michael O <michael.b.ohalloran@gmail.com>
2020-03-09 20:34:09 -0700

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae
+        asperiores tempore neque perspiciatis ex dicta recusandae corporis
+        dolorem eveniet esse! Repellendus ipsum temporibus, ducimus dolorem aut
+        minus nemo cupiditate veniam quaerat pariatur vero autem ipsa labore
+        quae quos commodi tempora beatae soluta recusandae ad in odio nihil sit.
+        Soluta ipsa maxime necessitatibus eveniet vero. Eligendi ad ex sequi
+        sed. Repellat, atque incidunt magnam laboriosam voluptatum
+        exercitationem officia placeat ullam consequuntur blanditiis.
+        Exercitationem minima molestiae sit, ipsa temporibus quo? Exercitationem
+        dolore excepturi provident aspernatur dolores maiores voluptates
+        molestiae debitis illum adipisci temporibus est magni, omnis suscipit
+        totam nam corrupti fugit sint.
+      </p>
+    </section>
+  );
+}
